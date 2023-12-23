a30b851de5e5acbdeefa661a04c26c0d88b9b646
LMikadaL <panupong.mikada@gmail.com>
2023-12-24 00:55:11 +0700

    update Bureaucrat class

--- CPP_05/ex02/Bureaucrat.hpp
+++ CPP_05/ex02/Bureaucrat.hpp
@@ -1,4 +1,39 @@
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
+# include <iostream>
+# include <exception>
+
+class Bureaucrat {
+	private:
+		const std::string	_name;
+		int 				_grade;
+
+	public:
+		Bureaucrat();
+		Bureaucrat(const Bureaucrat &b);
+		Bureaucrat(const std::string &name, const int &grade);
+		~Bureaucrat();
+		Bureaucrat& operator=(const Bureaucrat &b);
+
+		int			getGrade() const;
+		std::string	getName() const;
+
+		void		setGrade(const int &grade);
+
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char* what() const throw();
+		};
+
+		void		signForm(class Form &f) const;
+};
+
+std::ostream&	operator<<(std::ostream &out, const Bureaucrat &b);
+
 #endif
