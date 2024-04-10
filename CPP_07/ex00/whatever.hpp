146d674dc8b7acb85d58c4254bb1471d966cc3fb
Mikada <panupong.mikada@gmail.com>
2024-04-10 21:32:17 +0700

    update CPP_07/ex00

---
+++ CPP_07/ex00/whatever.hpp
@@ -0,0 +1,32 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T t;
+	
+	t = b;
+	b = a;
+	a = t;
+}
+
+template <typename T>
+T min(T const &a, T const &b)
+{
+	if (a < b)
+		return a;
+	return b;
+}
+
+template <typename T>
+T max(T const &a, T const &b)
+{
+	if (a > b)
+		return a;
+	return b;
+}
+
+#endif
