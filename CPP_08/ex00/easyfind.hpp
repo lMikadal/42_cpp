f3b30abfb205952737c0b106e9d7192f683d2a51
Mikada <panupong.mikada@gmail.com>
2024-05-07 21:38:59 +0700

    edit func

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -9,8 +9,10 @@ template <typename T>
 bool easyfind(T &container, int n)
 {
 	typename T::iterator it = std::find(container.begin(), container.end(), n);
+
 	if (it == container.end())
 		throw std::exception();
+
 	return true;
 }
 
